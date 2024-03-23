4cf4f6e5d629e1042064d5a71e3c34d1a34bf7cb
sohelikona <kritiaktar0@gmail.com>
2024-03-23 06:17:05 +0600

    🤗

--- Lexer.c
+++ Lexer.c
@@ -22,3 +22,12 @@ bool isOperator(char chr)
             || chr == '/' || chr == '>' || chr == '<'
             || chr == '='); 
 }
+
+bool isValidIdentifier(char* str) 
+{ 
+    return (str[0] != '0' && str[0] != '1' && str[0] != '2'
+            && str[0] != '3' && str[0] != '4'
+            && str[0] != '5' && str[0] != '6'
+            && str[0] != '7' && str[0] != '8'
+            && str[0] != '9' && !isDelimiter(str[0])); 
+} 
